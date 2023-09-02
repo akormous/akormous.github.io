725c1472ec603bfb2659d7fabca0e91a8d4acda8
akormous <iamakshatchauhan@gmail.com>
2023-09-03 00:37:36 +0530

    Update Navbar and Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,19 @@
 export function Hero() {
     return (
     <>
-    <div className="h-screen absolute top-0 left-0 overflow-hidden -z-10 brightness-75">
     <video 
-     autoPlay loop muted playsInline
+        className="absolute -z-20 brightness-75 left-2/4 top-2/4 h-full w-full object-cover -translate-y-2/4 -translate-x-2/4"
+        autoPlay loop muted playsInline
     >
         <source src="/hero4.mp4" type="video/mp4" />
     </video>
-    
+    <div className="absolute container flex flex-col items-center justify-center top-0 h-full w-full -z-10 px-6">
+      <h1 className="text-white xs:text-2xl sm:text-4xl md:text-6xl lg:text-9xl font-semibold">Hi, I&apos;m Akshat.</h1>
+      <p className="text-white md:text-sm lg:text-lg">Full-stack engineer crafting ideas into reality, bridging tech and business for startups and enterprises.</p>
     </div>
+    
+    
+       
     </>
     );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,17 @@
 export function Navbar() {
     return (
         
-            <div className="bg-transparent min-h-fit flex my-2 text-white">
-                <div className="flex items-center flex-grow">
+            <div className="bg-transparent h-auto grid grid-cols-3 p-6 items-center text-white">
+                <div className="flex justify-start">
                     <a className="mx-9 font-bold">Projects</a>
                     <a className="mx-9 font-bold">Experience</a>
                     <a className="mx-9 font-bold">Contact</a>
                 </div>
-                <h1 className="text-md lg:text-5xl text-white font-semibold">AC</h1>
-                <div className="flex items-center justify-end flex-grow text-white">
-                <a className="mx-9 font-bold">About</a>
+                <div className="flex justify-center">
+                <h1 className="text-3xl lg:text-5xl text-white font-semibold">AC</h1>
+                </div>
+                <div className="flex justify-end">
+                    <a className="mx-9 font-bold">About</a>
                     <a className="mx-9 font-bold">Github</a>
                     <a className="mx-9 font-bold">Hire</a>
                 </div>
