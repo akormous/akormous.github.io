b3f3e664947901f070cb701ed7da79aef7c9a5b5
akormous <iamakshatchauhan@gmail.com>
2023-09-09 21:26:16 +0530

    Add responsive navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,14 +2,14 @@ export function Hero() {
     return (
     <>
     <video 
-        className="absolute -z-20 brightness-75 left-2/4 top-2/4 h-full w-full object-cover -translate-y-2/4 -translate-x-2/4"
+        className="absolute -z-20 brightness-50 left-2/4 top-2/4 h-full w-full object-cover -translate-y-2/4 -translate-x-2/4"
         autoPlay loop muted playsInline
     >
         <source src="/hero4.mp4" type="video/mp4" />
     </video>
-    <div className="absolute container flex flex-col items-center justify-center top-0 h-full w-full -z-10 px-6">
-      <h1 className="text-white xs:text-2xl sm:text-4xl md:text-6xl lg:text-9xl font-semibold">Hi, I&apos;m Akshat.</h1>
-      <p className="text-white md:text-sm lg:text-lg">Full-stack engineer crafting ideas into reality, bridging tech and business for startups and enterprises.</p>
+    <div className="absolute h-screen w-full flex flex-col top-0 items-center justify-center -z-10 px-6">
+      <h1 className="text-white text-5xl md:text-7xl lg:text-9xl my-4">Hi, I&apos;m Akshat.</h1>
+      <p className="text-white text-md lg:text-lg">Full-stack engineer crafting ideas into reality, bridging tech and business for startups and enterprises.</p>
     </div>
     
     
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,44 @@
+'use client'
+import { useState } from "react";
+
 export function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+    const links = ["Projects","Experience","About","Contact"]
+
     return (
-        
-            <div className="bg-transparent h-auto grid grid-cols-3 p-6 items-center text-white">
-                <div className="flex justify-start">
-                    <a className="mx-9 font-bold">Projects</a>
-                    <a className="mx-9 font-bold">Experience</a>
-                    <a className="mx-9 font-bold">Contact</a>
+        <nav>
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 text-white">
+                <div className="flex items-center">
+                    <h1 className="text-3xl lg:text-5xl text-white font-semibold">AC</h1>
                 </div>
-                <div className="flex justify-center">
-                <h1 className="text-3xl lg:text-5xl text-white font-semibold">AC</h1>
+                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul className="flex flex-row">
+                        {links.map((link) => (
+                            <li key={link}>
+                                <a href="#" className="mx-9">{link}</a>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-                <div className="flex justify-end">
-                    <a className="mx-9 font-bold">About</a>
-                    <a className="mx-9 font-bold">Github</a>
-                    <a className="mx-9 font-bold">Hire</a>
+                <button type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm md:hidden" onClick={() => toggleMenu()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
+                <div className={`w-full block flex-grow md:hidden ${isOpen ? "block" : "hidden"}`}>
+                    <ul className="flex backdrop-blur-sm flex-col p-4 mt-4 border border-gray-100 rounded-lg ">
+                        {links.map((link) => (
+                            <li className="my-4" key={link}>
+                                <a href="#" className="mx-9">{link}</a>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
+                
             </div>
+        </nav>
     );
 }
